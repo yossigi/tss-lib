d1d85b5d7ab460b9f2d5d3bf195ae1e3f100009e
Jonathan Weiss <jonathan.weiss1@mail.huji.ac.il>
2024-08-06 13:04:03 +0300

    removed findRound for a parsedMessage

--- ecdsa/party/util.go
+++ ecdsa/party/util.go
@@ -26,35 +26,3 @@ func findProtocolType(message tss.ParsedMessage) protocolType {
 		return unknownProtocolType
 	}
 }
-
-func findRound(message tss.ParsedMessage) int {
-	switch message.Content().(type) {
-	case *signing.SignRound1Message1, *signing.SignRound1Message2:
-		return 1
-	case *signing.SignRound2Message:
-		return 2
-	case *signing.SignRound3Message:
-		return 3
-	case *signing.SignRound4Message:
-		return 4
-	case *signing.SignRound5Message:
-		return 5
-	case *signing.SignRound6Message:
-		return 6
-	case *signing.SignRound7Message:
-		return 7
-	case *signing.SignRound8Message:
-		return 8
-	case *signing.SignRound9Message:
-		return 9
-	case *keygen.KGRound1Message:
-		return 1
-	case *keygen.KGRound2Message1, *keygen.KGRound2Message2:
-		return 2
-	case *keygen.KGRound3Message:
-		return 3
-
-	default: // unrecognised message, just ignore!
-		return -1
-	}
-}
